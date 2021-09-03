d7bf0e97e82c700e6d5315e95b1bf24d7af9aa0c
Kim <gunslord@gmail.com>
2021-09-03 21:04:12 +0900

    링크태크적용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import MainPageComponent from "./main/index.js";
-
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 import "./App.css";
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path={"/"}>
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path={"/product/:id"}>
+          <ProductPage />
+        </Route>
+        <Route exact={true} path={"/upload"}>
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
